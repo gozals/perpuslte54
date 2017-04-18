ae7e3b2865a8bb43da5f01f2feb8006e64c6d3ff
Gozals25 <shailentcer@gmail.com>
2017-04-18 17:25:22 +0700

    add sluggable package

--- config/app.php
+++ config/app.php
@@ -178,7 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Barryvdh\Debugbar\ServiceProvider::class,
-
+        Cviebrock\EloquentSluggable\ServiceProvider::class,
 
     ],
 
