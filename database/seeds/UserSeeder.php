76c52937409f8346d8575b2269fbfcdfcae0559a
Gozals25 <shailentcer@gmail.com>
2017-04-18 12:05:53 +0700

    create UserSeeder, register it into DatabaseSeeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin')
+        ]);
+        User::create([
+            'name' => 'editor',
+            'email' => 'editor@editor.com',
+            'password' => bcrypt('editor')
+        ]);
+        User::create([
+            'name' => 'member',
+            'email' => 'member@member.com',
+            'password' => bcrypt('member')
+        ]);
+
+        $faker = \Faker\Factory::create();
+        foreach(range(1,10) as $index)
+        {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => bcrypt($faker->password)
+            ]);
+        }
+    }
+}
