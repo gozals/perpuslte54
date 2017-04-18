97d0fb8ef3046c3a74c4076da11b6e5ea9a85268
Gozals25 <shailentcer@gmail.com>
2017-04-18 17:04:50 +0700

    setup softedelete traits in Author Model

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Author extends Model
 {
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
     protected $table = 'authors';
 
     protected $fillable = ['name'];
