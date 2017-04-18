e1faa993a50a6210f072b7ace59369f2947da0e2
Gozals25 <shailentcer@gmail.com>
2017-04-18 17:43:47 +0700

    update Author model to use Sluggable traits

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -2,16 +2,27 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Author extends Model
 {
     use SoftDeletes;
+    use Sluggable;
 
     protected $dates = ['deleted_at'];
 
     protected $table = 'authors';
 
     protected $fillable = ['name'];
+
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
 }
