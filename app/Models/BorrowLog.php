9240524c84ee46d0af023af77cdad5d6d7057ed9
Gozals25 <shailentcer@gmail.com>
2017-04-19 17:53:32 +0700

    add query scope and type casting in BorrowLog model

--- app/Models/BorrowLog.php
+++ app/Models/BorrowLog.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class BorrowLog extends Model
 {
+    protected $fillable = ['book_id', 'user_id', 'is_returned'];
+
+    protected $casts = [
+        'is_returned' => 'boolean',
+    ];
+
+
     public function book()
     {
         return $this->belongsTo(Book::class);
@@ -15,4 +22,15 @@ class BorrowLog extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function scopeBorrowed($query)
+    {
+        return $query->where('is_returned', 0);
+    }
+
+    public function scopeReturned($query)
+    {
+        return $query->where('is_returned', 1);
+    }
+
 }
