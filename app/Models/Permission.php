951a57f5bccbc996d5197a3793a4fd3075cd0225
Gozals25 <shailentcer@gmail.com>
2017-04-19 18:42:29 +0700

    update RBAC Models

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -6,8 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
+
+    protected $fillable = ['name', 'display_name', 'description', 'route'];
+
     public function roles()
     {
         return $this->belongsToMany(Role::class);
     }
+
+    /**
+     * @param $roleName
+     *
+     * @return bool
+     */
+    public function hasRole($roleName)
+    {
+        foreach($this->roles as $role)
+        {
+            if($role->name == $roleName)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -16,4 +16,52 @@ class Role extends Model
     {
         return $this->belongsToMany(User::class);
     }
+
+    public function savePermissions($inputPermissions)
+    {
+        if (!empty($inputPermissions)) {
+            $this->permissions()->sync($inputPermissions);
+        } else {
+            $this->permissions()->detach();
+        }
+    }
+
+    public function attachPermission($permission)
+    {
+        if (is_object($permission)) {
+            $permission = $permission->getKey();
+        }
+
+        if (is_array($permission)) {
+            $permission = $permission['id'];
+        }
+
+        $this->permissions()->attach($permission);
+    }
+
+    public function detachPermission($permission)
+    {
+        if (is_object($permission))
+            $permission = $permission->getKey();
+
+        if (is_array($permission))
+            $permission = $permission['id'];
+
+        $this->permissions()->detach($permission);
+    }
+
+    public function attachPermissions($permissions)
+    {
+        foreach ($permissions as $permission) {
+            $this->attachPermission($permission);
+        }
+    }
+
+    public function detachPermissions($permissions)
+    {
+        foreach ($permissions as $permission) {
+            $this->detachPermission($permission);
+        }
+    }
+
 }
