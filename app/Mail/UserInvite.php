7978411d74facaafe39b4291e6e97a71a4de1197
Gozals25 <shailentcer@gmail.com>
2017-04-20 11:24:37 +0700

    add Mailble Class

---
+++ app/Mail/UserInvite.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class UserInvite extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public $member;
+    public $password;
+
+    public function __construct(User $member, $password)
+    {
+        $this->member = $member;
+        $this->password = $password;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('auth.emails.invite')
+            ->subject('Anda telah didaftarkan di Larapus!');
+    }
+}
